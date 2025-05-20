8b5718b554d2be2699b60dc4ba4e1a8bed03c831
Adreyan Distor <adreyandistor@gmail.com>
2025-05-19 17:05:22 -0700

    Update region_analysis_adist003.py

--- src/region_analysis_adist003.py
+++ src/region_analysis_adist003.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.functions import col, substring, when, make_date
+from pyspark.sql.functions import col, substring, when, make_date, avg, sum, max, min, count, mean
 from pyspark.sql import Row
 from pyspark.sql import udf
 from pyspark.sql.types import StringType
@@ -9,3 +9,4 @@ spark = SparkSession.builder.appName("Region Analysis").getOrCreate()
 
 df = spark.read.option('header','true').option('inferSchema','true').csv('/home/cs179g/USTCA/region_observations/*.csv')
 
+print(len(df.filter(df['region'] == 'Unknown')))
