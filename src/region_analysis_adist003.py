409003c2dae970cdedbcd328d223375aaf6a2025
Adreyan Distor <adreyandistor@gmail.com>
2025-05-19 18:41:35 -0700

    Update region_analysis_adist003.py

--- src/region_analysis_adist003.py
+++ src/region_analysis_adist003.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.functions import col, substring, when, make_date, avg, sum, max, min, count, mean
+from pyspark.sql.functions import col, substring, when, avg, sum, max, min, count, mean
+from pyspark.sql.functions import to_date, make_date, year 
 from pyspark.sql import Row
 from pyspark.sql import udf
 from pyspark.sql.types import StringType
@@ -9,8 +10,12 @@ spark = SparkSession.builder.appName("Region Analysis").getOrCreate()
 
 df = spark.read.option('header','true').option('inferSchema','true').csv('/home/cs179g/USTCA/region_observations/*.csv')
 
-print(df.filter(df['region'] == 'Unknown').count())
-
 #df = df.withColumn()
 
-region_df = df.filter(df['value']  == 'TMAX')
+temp_df = df.filter(df['value']  == 'TMAX')
+temp_df = temp_df.withColumn('date', to_date(temp_df['date'], 'yy-MM-dd'))
+temp_df = temp_df.withColumn('year', year(temp_df['date']))
+
+region_df = temp_df.groupBy('region', 'year')
+
+region_df.show
