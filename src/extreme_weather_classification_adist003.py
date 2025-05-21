1c538873305e9cf62ffb8545e8fc2fa46ce816f7
Adreyan Distor <adreyandistor@gmail.com>
2025-05-20 18:05:26 -0700

    Update extreme_weather_classification_adist003.py

--- src/extreme_weather_classification_adist003.py
+++ src/extreme_weather_classification_adist003.py
@@ -11,53 +11,36 @@ spark = SparkSession.builder.appName("Extreme Weather Classification").getOrCrea
 file_path = "/home/cs179g/USTCA/data/region_observations_sample/*.csv"
 
 main_df = spark.read.format("csv").option("header","true").load(file_path)
-
 def is_extreme_weather(value_name, value):
-    if value is None:
-        return False
-    value = float(value)
-    value_name = str(value_name)
-    
-    if value_name == 'TMAX':
-        value = value/10
-        if value  > 35: # 35 C
-            return True
-        else:
-            return False
+    try:
+        if value is None or value_name is None:
+            return "False"
+        value = float(value)
+        value_name = str(value_name)
         
-    elif value_name == 'TMIN':
-        value = value/10
-        if value < -18: # 18 C
-            return True
+        if value_name == 'TMAX':
+            value = value / 10
+            return "True" if value > 35 else "False"
+        elif value_name == 'TMIN':
+            value = value / 10
+            return "True" if value < -18 else "False"
+        elif value_name == 'SNWD':
+            return "True" if value > 500 else "False"
+        elif value_name == 'SNOW':
+            return "True" if value > 250 else "False"
+        elif value_name == 'PRCP':
+            return "True" if value > 50 else "False"
         else:
-            return False
+            return "False"
+    except:
+        return "False"
 
-    elif value_name == 'SNWD':
-        if value > 500: # 500nm (20 inches)
-            return True
-        else:
-            return False
-    elif value_name == 'SNOW':
-        if value > 250: # 250nm (10 inches)
-            return True
-        else:
-            return False
-    elif value_name == 'PRCP':
-        if value > 50: # 50nm (2 inches)
-            return True
-        else:
-            return False
-    return None
+classify_extreme_weather = F.udf(is_extreme_weather, StringType())
 
+weather_df = main_df.withColumn('is_extreme_weather', classify_extreme_weather(col('element'), col('value')))
 
+weather_df.filter(col('is_extreme_weather') == 'True').show()
 
-classify_extreme_weather = F.udf(main_df, StringType())
+weather_df.write.csv('data/extreme_weather_observations', header=True, mode='overwrite')
 
-weather_df = main_df.withColumn('is_extreme_weather', classify_extreme_weather(main_df['element'], main_df['value']))     
-
-weather_df.show()
-
-weather_df.write.csv('extreme_weather_observations.csv', header=True, mode='overwrite')
-
-# Stop the spark job
 spark.stop()
